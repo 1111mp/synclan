ac1759a0b05ac984efbb552b3fa81ce2d3344d50
The1111mp <The1111mp@outlook.com>
2025-09-15 19:56:11 +0800

    chore: toogle link plugin
    Signed-off-by: The1111mp <The1111mp@outlook.com>

--- src/components/nodes/index.ts
+++ src/components/nodes/index.ts
@@ -1,5 +1,6 @@
 export * from './code-plus-node';
 export * from './emoji-node';
+export * from './pre-link';
 export * from './simple-list-node';
 export * from './simple-list-item-node';
 export * from './simple-quote-node';
--- src/components/plugins/index.ts
+++ src/components/plugins/index.ts
@@ -5,6 +5,8 @@ export * from './code-high-light-shiki-plugin';
 export * from './code-node-toolbar-pligin';
 export * from './emoji-picker-plugin';
 export * from './enter-behavior-plugin';
+export * from './floating-link-editor-plugin';
 export * from './floating-text-format-toolbar-plugin';
 export * from './is-empty-plugin';
 export * from './is-focused-plugin';
+export * from './link-plugin';
