df4126c3b7ebefad10e487db24a41d69e7c26edb
The1111mp <The1111mp@outlook.com>
2026-07-20 13:45:05 +0800

    chore: release v0.1.0-rc
    Signed-off-by: The1111mp <The1111mp@outlook.com>

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,8 +3,6 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  '**/*.{js,mjs,cjs,ts,jsx,tsx,md,html,css}': [
-    'oxfmt --write',
-    'oxlint --type-aware',
-  ],
+  '**/*.{js,mjs,cjs,ts,jsx,tsx}': ['oxfmt --write', 'oxlint --type-aware'],
+  '**/*.{md,html,css}': ['oxfmt --write'],
 };
