96c61933cdb63c7fb3a454948a9b0399d76bf321
The1111mp <The1111mp@outlook.com>
2026-07-25 13:55:22 +0800

    fix: add image preview for image messages
    Signed-off-by: The1111mp <The1111mp@outlook.com>

--- src/components/messages/message-image.tsx
+++ src/components/messages/message-image.tsx
@@ -1,3 +1,4 @@
+import { useImagePreview } from '@/components';
 import { calculateMediaSize, getMediaUrl } from '@/lib/media';
 
 import { parseMessageExtra } from './util';
@@ -7,16 +8,37 @@ type Props = {
 };
 
 function ImageMessage({ message }: Props) {
+  const { openPreview } = useImagePreview();
+
   const extra = parseMessageExtra<MediaMessageExtra>(message.extra);
 
   const { width, height } = calculateMediaSize(extra?.width, extra?.height);
+
+  const onImagePreview = () => {
+    if (!message.content) return;
+
+    const container = document.querySelector('#synclan-device-message-list');
+    if (!container) return;
+
+    const images = Array.from(
+      container.querySelectorAll('img[data-synclan-preview-image]'),
+    )
+      .map((img) => img.getAttribute('src'))
+      .filter((src) => src !== null);
+
+    const index = images.indexOf(getMediaUrl(message.content));
+
+    openPreview(images, index);
+  };
 
   return (
     <div style={{ width, height }}>
       <img
         loading='lazy'
+        data-synclan-preview-image
         src={getMediaUrl(message.content)}
         className='h-full w-full rounded-md object-contain'
+        onClick={onImagePreview}
       />
     </div>
   );
